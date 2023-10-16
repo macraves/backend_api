f98635d8b89e9f48f1f365c73544c9237b84bf7f
macraves <macius.caner@gmail.com>
2023-10-16 02:43:29 +0100

    post, delete,put,customerror added

--- FLASK-APIs/backend/backend_app.py
+++ FLASK-APIs/backend/backend_app.py
@@ -1,6 +1,7 @@
 """Api for the backend app."""
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
+from backend_methods import add_post, validate_post
 
 app = Flask(__name__)
 CORS(app)  # This will enable CORS for all routes
@@ -11,10 +12,66 @@ POSTS = [
 ]
 
 
+class CustomError(Exception):
+    """Custom error class."""
+
+    def __init__(self, message, status_code):
+        self.message = message
+        self.status_code = status_code
+
+
 @app.route("/api/posts", methods=["GET"])
 def get_posts():
     """Return all posts."""
     return jsonify(POSTS)
+
+
+@app.route("/api/posts", methods=["POST"])
+def handle_posts():
+    """Add a new post if post is valid."""
+    received_data = request.get_json()
+    post = add_post(received_data, POSTS)
+    if post:
+        POSTS.append(post)
+        return jsonify(POSTS)
+    raise CustomError({"message": "Bad Data Structure", "method": "PUT"}, 400)
+
+
+@app.route("/api/posts/<int:post_id>", methods=["DELETE"])
+def delete_post(post_id):
+    """Delete post by id."""
+    post = next((post for post in POSTS if post["id"] == post_id), None)
+    if post:
+        POSTS.remove(post)
+        return jsonify(POSTS)
+    raise CustomError({f"{post_id}": "Invalid delete request"}, 404)
+
+
+@app.route("/api/posts", methods=["PUT"])
+def update_post(post_id):
+    """Update post by id."""
+    received_post = request.get_json()
+    valid_post = validate_post(received_post, POSTS)
+    post = next((post for post in POSTS if post["id"] == post_id), None)
+    if post and valid_post:
+        pop_post = POSTS.pop(POSTS.index(post))
+        pop_post = valid_post
+        POSTS.append(pop_post)
+        return jsonify(POSTS)
+    raise CustomError(
+        {"message": "Put object either invalid or id coul not find", "method": "PUT"},
+        404,
+    )
+
+
+@app.errorhandler(CustomError)
+def handle_custom_error(error):
+    "custom error handler"
+    response = {
+        "message": error.message,
+        "status_code": error.status_code,
+    }
+    return jsonify(response), error.status_code
 
 
 if __name__ == "__main__":
---
+++ FLASK-APIs/backend/backend_methods.py
@@ -0,0 +1,44 @@
+"""Backend methods for the Flask API."""
+
+
+def generate_new_id(posts: list):
+    """Finds max value in posts and assigns max + 1"""
+    if len(posts) > 0:
+        return max(post["id"] for post in posts) + 1
+    return 1
+
+
+def format_post_strings(post: dict):
+    """Ignores whitespaces and applies title method"""
+    title = post.get("title", "").strip().title()
+    content = post.get("content", "").strip().title()
+    post_id = post.get("id")
+    return {"id": post_id, "title": title, "content": content}
+
+
+def validate_post(post: dict, posts):
+    """validatation for the post"""
+    if isinstance(post, dict) and "title" in post and "content" in post:
+        # Check if post has "id" key
+        if post.get("id") is None:
+            # Generate a new id
+            post["id"] = generate_new_id(posts)
+            return format_post_strings(post)
+        else:
+            # In here post has "id" key
+            post_id = post.get("id")
+            if isinstance(post_id, int):
+                if next((post for post in posts if post["id"] == post_id), None):
+                    # Get new id
+                    post_id = generate_new_id(posts=posts)
+            return format_post_strings(post)
+    return None
+
+
+def add_post(post: dict, posts: list):
+    """Checks if it is valid
+    add a unique id and return the post"""
+    is_valid_post = validate_post(post, posts)
+    if is_valid_post:
+        return is_valid_post
+    return None
